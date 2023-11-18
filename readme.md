c07fbd50ca493bbdf27d59475300352868a43d09
NGUYEN Viet Anh <nguyenvietanh101010@gmail.com>
2023-11-18 11:08:32 +0100

    First commit for VA

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Agent Tourist
+- Send 
+
+# Agent Guider
--- src/main/java/GuideTourist/AgentTouristique.java
+++ src/main/java/GuideTourist/AgentTouristique.java
@@ -1,4 +1,12 @@
 package GuideTourist;
 
+import jade.core.AID;
+
 public class AgentTouristique {
+    private static final long serialVersionUID = 1L;
+
+    private String targetBookTitle;
+    // The list of known seller agents
+    private AID[] sellerAgents;
+
 }
